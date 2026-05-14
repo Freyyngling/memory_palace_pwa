bd9f6c8c33856321a23db67e9e762c7718772dbc
Freyyngling <102222351+Freyyngling@users.noreply.github.com>
2026-05-14 17:20:20 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,14 @@
+# Memory Palace Trainer PWA
+
+## 使い方
+
+1. index.html をブラウザで開く
+2. または GitHub Pages にアップロード
+3. スマホで開き「ホーム画面に追加」
+
+## ファイル説明
+
+- index.html … メインアプリ
+- manifest.json … PWA設定
+- service-worker.js … オフライン対応
+- icon-192.png / icon-512.png … アプリアイコン
---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'memory-palace-v1';
+const URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
